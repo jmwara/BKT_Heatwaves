36545d63ace91e752ddefc35282802304a217630
jmwara <135848576+jmwara@users.noreply.github.com>
2024-08-20 14:57:06 -0400

    Update 04-1_Salmon_WGCNA_NetworkConstruction_Auto.R

--- 04-1_Salmon_WGCNA_NetworkConstruction_Auto.R
+++ 04-1_Salmon_WGCNA_NetworkConstruction_Auto.R
@@ -1,7 +1,7 @@
 #read in libraries
 library(WGCNA)
 library(data.table)
-salmon_data <- fread("~/scratch/BrookTrout_Analyses/WGCNA/salmon_logcounts_environ.txt", drop=1) #read in dataset with environmental and expression data
+salmon_data <- fread("PATH_TO_YOUR_DIRECTORY/salmon_logcounts_environ.txt", drop=1) #read in dataset with environmental and expression data
 enviro_data <- salmon_data[,1:16] #create a separate data frame with just environmental data
 express_data <- salmon_data[,-(1:16)] #create a separate data frame with just expression data
 rownames(express_data) <- salmon_data$Sample_ID #name the expression data rows with sample IDs so future analyses of the expression data can be aligned to environmental data
@@ -12,4 +12,4 @@ moduleLabels <- net$colors #save an object with the color names as the labels fo
 moduleColors <- labels2colors(net$colors) #create a copy of the color names reformatted so they can print colors
 MEs <- net$MEs #save module eigengene values to a dataframe
 geneTree <- net$dendrograms[[1]] #save the dendrogram of transcripts
-save(MEs, moduleLabels, moduleColors, geneTree, file="~/scratch/BrookTrout_Analyses/WGCNA/BrookTroutHeatwave_netwrkConstruction_auto.RData") #save these results to an .RData file 
+save(MEs, moduleLabels, moduleColors, geneTree, file="PATH_TO_YOUR_DIRECTORY/BrookTroutHeatwave_netwrkConstruction_auto.RData") #save these results to an .RData file 
